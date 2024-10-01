099e809263f46d2be8f4cc4cc87fbf2620b6bcd1
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-01 20:38:02 +0100

    added skills page with animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    // Run the function once on init to check for elements already in view
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkScroll();
+  }
+
+  checkScroll(): void {
+    const elements = document.querySelectorAll('.item'); // Elements to animate
+    const windowHeight = window.innerHeight;
+
+    elements.forEach((el) => {
+      const elementTop = el.getBoundingClientRect().top;
+
+      // Check if element is in view
+      if (elementTop < windowHeight - 100) { // Adjust this offset as needed
+        el.classList.add('animate');
+      }
+    });
+  }
 }
