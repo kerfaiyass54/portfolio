7ffd25c987040fc6835ffb4dde950e314dc74999
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-03 13:44:37 +0100

    finished app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,23 +6,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    {
-        path: 'home', component:HomeComponent
-    },
-    {
-        path: 'contact', component:ContactComponent
-    },
-    {
-        path: 'skills', component:SkillsComponent
-    },
-    {
-        path: 'projects', component:ProjectsComponent
-    },
-    {
-        path: 'about', component:AboutComponent
-    },
-    { path: '', redirectTo: '/about', pathMatch: 'full' }, 
-    { path: '**', redirectTo: '/about' } 
+
 ];
 
 
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,19 +1,57 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from "../about/about.component";
+import { SkillsComponent } from "../skills/skills.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
+
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [NgbModule],
+  imports: [NgbModule, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  isHome = true;
+  isProjects = false;
+  isSkills = false;
+  isContact = false;
 
   constructor(private router:Router){}
   navigateTo(path: string) {
     this.router.navigate([path]);
   }
+
+  homePage(){
+    this.isHome=true;
+    this.isProjects = false;
+    this.isSkills = false;
+    this.isContact = false;
+  }
+
+  projectPage(){
+    this.isHome=false;
+    this.isProjects = true;
+    this.isSkills = false;
+    this.isContact = false;
+  }
+
+  skillsPage(){
+    this.isHome=false;
+    this.isProjects = false;
+    this.isSkills = true;
+    this.isContact = false;
+  }
+
+  contactPage(){
+    this.isHome=false;
+    this.isProjects = false;
+    this.isSkills = false;
+    this.isContact = true;
+  }
  
 
-}
+}import { CommonModule } from '@angular/common';
+
