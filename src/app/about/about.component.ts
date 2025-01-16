c812ae925c1ccfe25e9f8be239deb5681e8d4146
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-01-16 21:35:26 +0100

    Fixed bootstrap problem

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,26 +12,10 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // Run the function once on init to check for elements already in view
-    this.checkScroll();
+
   }
 
-  @HostListener('window:scroll', [])
-  onWindowScroll(): void {
-    this.checkScroll();
-  }
 
-  checkScroll(): void {
-    const elements = document.querySelectorAll('.item'); // Elements to animate
-    const windowHeight = window.innerHeight;
 
-    elements.forEach((el) => {
-      const elementTop = el.getBoundingClientRect().top;
 
-      // Check if element is in view
-      if (elementTop < windowHeight - 100) { // Adjust this offset as needed
-        el.classList.add('animate');
-      }
-    });
-  }
 }
