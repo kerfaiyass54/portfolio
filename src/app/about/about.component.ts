ab482f5bfbd896503f3f42c8a2769d751dcccc3d
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-01-19 21:53:41 +0100

    Added some animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,11 +9,39 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './about.component.css'
 })
 export class AboutComponent implements OnInit {
+  aboutMe = false;
+  volunteer = false;
+  educ = false;
   constructor() { }
 
   ngOnInit(): void {
 
+
+
   }
+
+
+  showAbout(){
+    this.aboutMe = true;
+    this.volunteer = false;
+    this.educ = false;
+  }
+
+  showEduc(){
+    this.aboutMe = false;
+    this.volunteer = true;
+    this.educ = false;
+  }
+
+  showVolunteer(){
+    this.aboutMe = false;
+    this.volunteer = false;
+    this.educ = true;
+  }
+
+
+
+
 
 
 
