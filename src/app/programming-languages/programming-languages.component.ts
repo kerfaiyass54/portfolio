04b663bfb5cebe504a22082b6c0a4084b2579814
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-05-23 23:11:43 +0100

    Added frameworks

--- src/app/programming-languages/programming-languages.component.ts
+++ src/app/programming-languages/programming-languages.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbPopover} from "@ng-bootstrap/ng-bootstrap";
+
 
 @Component({
   selector: 'app-programming-languages',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbPopover
+  ],
   templateUrl: './programming-languages.component.html',
   styleUrl: './programming-languages.component.css'
 })
-export class ProgrammingLanguagesComponent {
+export class ProgrammingLanguagesComponent implements OnInit{
+
+  ngOnInit(){
+
+  }
 
 }
