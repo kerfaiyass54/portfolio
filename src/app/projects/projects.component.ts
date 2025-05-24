e574f62cb14b18e3b53b853e75a197ff45e85419
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-05-24 09:21:32 +0100

    Started side-project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {AboutMeSectionComponent} from "../about-me-section/about-me-section.component";
+import {EducatinSectionComponent} from "../educatin-section/educatin-section.component";
+import {NgIf} from "@angular/common";
+import {VolunteeringSectionComponent} from "../volunteering-section/volunteering-section.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+    imports: [
+        AboutMeSectionComponent,
+        EducatinSectionComponent,
+        NgIf,
+        VolunteeringSectionComponent
+    ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  noButton = true;
+  side = false;
+  academic = false;
 
+  showPersonal() {
+
+  }
+
+  showAcademic() {
+
+  }
 }
