243c910e0061a617c96316e96fc32a8bbb3c0bb4
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-05-24 09:15:25 +0100

    Added projects components

---
+++ src/app/academic-projects/academic-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-academic-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './academic-projects.component.html',
+  styleUrl: './academic-projects.component.css'
+})
+export class AcademicProjectsComponent {
+
+}
---
+++ src/app/side-projects/side-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-projects.component.html',
+  styleUrl: './side-projects.component.css'
+})
+export class SideProjectsComponent {
+
+}
