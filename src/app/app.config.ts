a35fd7840bf1ae4c04ea3dd58280c813e03d25fe
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-08-23 10:54:49 +0100

    Configured translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {TranslateLoader} from "@ngx-translate/core";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader();
+}
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    }
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-/// <reference types="@angular/localize" />
+
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes'
 import { AppComponent } from './app/app.component';
 
+
+
+
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes) // Provide the router with your routes
+    provideRouter(routes)
   ]
 }).catch(err => console.error(err));
